d905efa309ae68649ee4972c818c2f9664f65f83
CallieAnn <dedorec@my.lanecc.edu>
2018-11-03 14:11:57 -0700

    finished 4 homecontroller tests for stories/comments

--- PattonFanSite.Tests/HomeControllerTests.cs
+++ PattonFanSite.Tests/HomeControllerTests.cs
@@ -3,6 +3,7 @@ using Xunit;
 using PattonFanSite.Models;
 using PattonFanSite.Controllers;
 using PattonFanSite.Repositories;
+using System.Collections.Generic;
 
 namespace PattonFanSite.Tests
 {
@@ -33,19 +34,53 @@ namespace PattonFanSite.Tests
         [Fact]
         public void Stories()
         {
+            //tests to see that the view receives a list of stories sorted by title
+            var repo = new FakeStoryRepository();
+            var controller = new HomeController(repo);
+
+            var result = controller.Stories();
+            var model = ((Microsoft.AspNetCore.Mvc.ViewResult)result).Model as List<Story>;
+
+            Assert.Equal("Guess Who Ate the Pansies", model[0].Title);
 
         }
 
         [Fact]
         public void AddComment()
         {
+            var repo = new FakeStoryRepository();
+            var controller = new HomeController(repo);
+
+            Story s = repo.GetStoryByTitle("Patton's first day");
+            
+
+            var result = controller.AddComment(s.Title);
+            var model = ((Microsoft.AspNetCore.Mvc.ViewResult)result).Model as String;
+
+            Assert.Equal("Patton's first day", model);
 
         }
 
         [Fact]
         public void PostAddComment()
         {
+            var repo = new FakeStoryRepository();
+            var controller = new HomeController(repo);
 
+            User u = new User
+            {
+                Name = "Fake User"
+            };
+
+            Comment c = new Comment()
+            {
+                CommentText = "Test Comment",
+                Contributor = u,
+            };
+
+            controller.AddComment("Patton's first day", c.CommentText, c.Contributor.Name);
+
+            Assert.Equal("Fake User", repo.Stories[0].Comments[0].Contributor.Name);
         }
     }
 }
